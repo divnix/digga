831f9b1b5c2a1edf14428df3c6792c82350b1bcd
Timothy DeHerrera <tim.deh@pm.me>
2019-12-16 02:41:55 -0700

    misc#make-linux-fast-again: init

--- profiles/misc/default.nix
+++ profiles/misc/default.nix
@@ -3,5 +3,6 @@
   imports = [
     ./stubby.nix
     ./adblocking.nix
+    ./make-linux-fast-again.nix
   ];
 }
---
+++ profiles/misc/make-linux-fast-again.nix
@@ -0,0 +1,14 @@
+# file: make-linux-fast-again.nix
+{ pkgs, config, ... }:
+let
+  inherit (builtins) readFile fetchurl;
+  cmdline = readFile (
+    fetchurl {
+      url = "https://make-linux-fast-again.com";
+      sha256 = "sha256:10diw5xn5jjx79nvyjqcpdpcqihnr3y0756fsgiv1nq7w28ph9w6";
+    }
+  );
+in
+{
+  boot.kernelParams = pkgs.lib.splitString " " cmdline;
+}
