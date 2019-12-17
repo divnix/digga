1e45e959bede2b58a0eb1209c97a64ef751cbfd6
Timothy DeHerrera <tim.deh@pm.me>
2019-12-16 21:15:48 -0700

    hp500281: add some basics

--- configurations/hp500281.nix
+++ configurations/hp500281.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 let
   inherit (builtins) readFile;
 in
@@ -6,8 +6,24 @@ in
   imports = [
     ../users/nrd.nix
     ../profiles/graphical
+    ../profiles/misc
   ];
 
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "ehci_pci"
+    "ahci"
+    "usbhid"
+    "sd_mod"
+  ];
+
+  boot.kernelModules = [ "kvm-intel" ];
+
+  networking.networkmanager = {
+    enable = true;
+  };
+
+  nix.maxJobs = lib.mkDefault 4;
 
   boot.loader.systemd-boot = {
     enable = true;
