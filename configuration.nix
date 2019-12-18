40fe0972c67424c7f13b816d2215c5c819ed2e9f
Timothy DeHerrera <tim.deh@pm.me>
2019-12-17 17:29:20 -0700

    add configuration.nix for compatibility
    recreates an impure representation of the current flake profile based
    on the systems current hostname.

---
+++ configuration.nix
@@ -0,0 +1,20 @@
+# this file is an impure recreation of the flake profile currently deployed
+# based on the systems hostname. The purpose is so tools which do not yet have
+# flake support (e.g `nixos-option`), can work as expected.
+{ lib, ... }:
+let
+  host = lib.fileContents /etc/hostname;
+in
+{
+  imports = [
+    "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/master.tar.gz}/nixos"
+    /etc/nixos/profiles/core.nix
+    "/etc/nixos/configurations/${host}.nix"
+  ];
+
+  networking.hostName = host;
+  nix.nixPath = [
+    "nixpkgs=${<nixpkgs>}"
+    "nixos-config=/etc/nixos/configuration.nix"
+  ];
+}
