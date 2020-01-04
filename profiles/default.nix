916add29213ce58152706b8b1a1ff12836e8f4a6
Timothy DeHerrera <tim.deh@pm.me>
2020-01-03 21:30:27 -0700

    profiles: export additional subprofiles

--- profiles/default.nix
+++ profiles/default.nix
@@ -3,6 +3,9 @@
   sway = import ./graphical/sway;
   qutebrowser = import ./graphical/qutebrowser;
   develop = import ./develop;
+  zsh = import ./develop/zsh;
+  tmux = import ./develop/tmux;
+  kakoune = import ./develop/kakoune;
   misc = import ./misc;
   games = import ./game;
 }
