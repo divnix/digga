0bf18acdd47879c81f10f7827db7a665613f2b6c
Timothy DeHerrera <tim.deh@pm.me>
2020-01-06 23:44:54 -0700

    virt: init profile

---
+++ profiles/virt/default.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }: {
+  virtualisation.libvirtd = {
+    enable = true;
+    qemuRunAsRoot = false;
+  };
+
+  # you'll need to add your user to 'libvirtd' group to use virt-manager
+  environment.systemPackages = with pkgs; [ virt-manager ];
+
+  nixpkgs.overlays = let
+    overlay = final: prev: {
+
+      # Patch libvirt to use ebtables-legacy
+      libvirt = if prev.libvirt.version <= "5.4.0" && prev.ebtables.version
+      > "2.0.10-4" then
+        prev.libvirt.overrideAttrs (oldAttrs: rec {
+          EBTABLES_PATH = "${final.ebtables}/bin/ebtables-legacy";
+        })
+      else
+        prev.libvirt;
+    };
+  in [ overlay ];
+}
+
