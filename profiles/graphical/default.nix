dc230fa066d2c41b708c938d178c193269a241b3
Timothy DeHerrera <tim.deh@pm.me>
2020-06-14 12:46:44 -0600

    Revert "graphical: disable pulse, use alsa instead"
    This reverts commit 8706a1ca533082673e2a96e060e1fbcff60b6bf5.

--- profiles/graphical/default.nix
+++ profiles/graphical/default.nix
@@ -5,7 +5,7 @@ in {
 
   hardware.opengl.enable = true;
   hardware.opengl.driSupport = true;
-  hardware.pulseaudio.enable = false;
+  hardware.pulseaudio.enable = true;
 
   environment = {
     etc = {
