51ef800af6071ffb13c1c44d733572b5090f55fe
Timothy DeHerrera <tim.deh@pm.me>
2020-06-25 21:52:40 -0600

    retroarch: 1.8.8 -> 1.8.9

--- overlays/retroarch.nix
+++ overlays/retroarch.nix
@@ -1,11 +1,11 @@
 final: prev: {
   retroarchBare = prev.retroarchBare.overrideAttrs (o: rec {
-    version = "1.8.8";
+    version = "1.8.9";
 
     src = prev.fetchFromGitHub {
       owner = "libretro";
       repo = "RetroArch";
-      hash = "sha256-rPMaTs98dmP0nkuHJRt5DmCgzk9imfJ+rgW9zfEqUFA=";
+      hash = "sha256-1kiq2ZobaPIhsWviOPCmDM3oJ0wJLmvYZ9PaqywF8I0=";
       rev = "v${version}";
     };
   });
