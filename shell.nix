1870fa29bd8e73cfe371255345c8883b26dc2319
Timothy DeHerrera <tim.deh@pm.me>
2020-08-02 15:24:00 -0600

    Revert "shell.nix: remove `rebuild` command"
    This reverts commit b954963e36af24dad462ad36146d581a162cf509.

--- shell.nix
+++ shell.nix
@@ -1,14 +1,24 @@
 { pkgs ? import <nixpkgs> { } }:
 let
   configs = "${toString ./.}#nixosConfigurations";
+  hostname = pkgs.lib.fileContents /etc/hostname;
+  build = "config.system.build";
 
-  buildIso = pkgs.writeShellScriptBin "build-iso" ''
-    nix build ${configs}.niximg.config.system.build.isoImage $@
+  rebuild = pkgs.writeShellScriptBin "rebuild" ''
+    if [[ -z $1 ]]; then
+      echo "Usage: $0 [host] {switch|boot|test|iso}"
+    elif [[ $1 == "iso" ]]; then
+      nix build ${configs}.niximg.${build}.isoImage
+    elif [[ -z $2 ]]; then
+      sudo -E nix shell -vv ${configs}.${hostname}.${build}.toplevel -c switch-to-configuration $1
+    else
+      sudo -E nix shell -vv ${configs}.$1.${build}.toplevel -c switch-to-configuration $2
+    fi
   '';
 in
 pkgs.mkShell {
   name = "nixflk";
-  nativeBuildInputs = with pkgs; [ git git-crypt nixFlakes buildIso ];
+  nativeBuildInputs = with pkgs; [ git git-crypt nixFlakes rebuild ];
 
   shellHook = ''
     mkdir -p secrets
