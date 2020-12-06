01f63a83ff513b15bd8e4fba3d643b010fb76e19
Timothy DeHerrera <tim.deh@pm.me>
2020-12-05 18:11:10 -0700

    redshift: point to latest wayland pr

--- overlays/redshift.nix
+++ overlays/redshift.nix
@@ -2,10 +2,10 @@ final: prev: {
   # with sway/wayland support
   redshift = prev.redshift.overrideAttrs (o: {
     src = prev.fetchFromGitHub {
-      owner = "CameronNemo";
+      owner = "minus7";
       repo = "redshift";
-      rev = "39c162ca487a59857c2eac231318f4b28855798b";
-      sha256 = "1in27draskwwi097wiam26bx2szcf58297am3gkyng1ms3rz6i58";
+      rev = "wayland";
+      hash = "sha256-pyR7xNdi/83NSoC2WIrX8U+A6nU+vZBlePLXrQZnc1k=";
     };
   });
 }
