2a53745f3e5f52b058882036136d4145dbc56ab6
Timothy DeHerrera <tim.deh@pm.me>
2020-12-19 17:22:00 -0700

    virt: enable oci containers via podman

--- profiles/virt/default.nix
+++ profiles/virt/default.nix
@@ -1,7 +1,21 @@
 { pkgs, ... }: {
-  virtualisation.libvirtd = {
-    enable = true;
-    qemuRunAsRoot = false;
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemuRunAsRoot = false;
+      allowedBridges = [
+        "virbr0"
+        "virbr1"
+      ];
+    };
+
+    podman = {
+      enable = true;
+      dockerCompat = true;
+    };
+
+    oci-containers.backend = "podman";
+
   };
 
   # you'll need to add your user to 'libvirtd' group to use virt-manager
