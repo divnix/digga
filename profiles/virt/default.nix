89d7829169395601307db3b4e3c4ba7feb03f6d4
Timothy DeHerrera <tim.deh@pm.me>
2020-12-25 15:57:47 -0700

    virt: use docker alias so completion works

--- profiles/virt/default.nix
+++ profiles/virt/default.nix
@@ -9,18 +9,14 @@
       ];
     };
 
-    podman = {
-      enable = true;
-      dockerCompat = true;
-    };
-
+    podman.enable = true;
     oci-containers.backend = "podman";
-
   };
 
   # you'll need to add your user to 'libvirtd' group to use virt-manager
   environment.systemPackages = with pkgs; [ virt-manager vagrant ];
 
+  environment.shellAliases.docker = "podman";
 
   environment.sessionVariables = {
     VAGRANT_DEFAULT_PROVIDER = "libvirt";
