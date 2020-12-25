3300b5d582db7f92c65cba16a9147256568711ef
Timothy DeHerrera <tim.deh@pm.me>
2020-12-25 12:53:57 -0700

    flake: clean up by moving implementation to utils
    Fixes #28 by adding an `externOverlays` list to easily import overlays
    from external flakes.

--- lib/utils.nix
+++ lib/utils.nix
@@ -2,7 +2,8 @@
 let
   inherit (builtins) attrNames isAttrs readDir listToAttrs;
 
-  inherit (lib) filterAttrs hasSuffix mapAttrs' nameValuePair removeSuffix;
+  inherit (lib) filterAttrs hasSuffix mapAttrs' nameValuePair removeSuffix
+    recursiveUpdate genAttrs;
 
   # mapFilterAttrs ::
   #   (name -> value -> bool )
@@ -13,21 +14,11 @@ let
   # Generate an attribute set by mapping a function over a list of values.
   genAttrs' = values: f: listToAttrs (map f values);
 
-in
-{
-  inherit mapFilterAttrs genAttrs';
-
-  recImport = { dir, _import ? base: import "${dir}/${base}.nix" }:
-    mapFilterAttrs
-      (_: v: v != null)
-      (n: v:
-        if n != "default.nix" && hasSuffix ".nix" n && v == "regular"
-        then
-          let name = removeSuffix ".nix" n; in nameValuePair (name) (_import name)
-
-        else
-          nameValuePair ("") (null))
-      (readDir dir);
+  pkgImport = { pkgs, system, overlays }:
+    import pkgs {
+      inherit system overlays;
+      config = { allowUnfree = true; };
+    };
 
   # Convert a list to file paths to attribute set
   # that has the filenames stripped of nix extension as keys
@@ -37,5 +28,69 @@ in
       name = removeSuffix ".nix" (baseNameOf path);
       value = import path;
     });
+
+in
+{
+  inherit mapFilterAttrs genAttrs' pkgImport pathsToImportedAttrs;
+
+  genPkgset = { master, nixos, overlays, system }:
+    {
+      osPkgs = pkgImport {
+        inherit system overlays;
+        pkgs = nixos;
+      };
+
+      unstablePkgs = pkgImport {
+        inherit system overlays;
+        pkgs = master;
+      };
+    };
+
+  overlayPaths =
+    let
+      overlayDir = ../overlays;
+      fullPath = name: overlayDir + "/${name}";
+    in
+    map fullPath (attrNames (readDir overlayDir));
+
+  recImport = { dir, _import ? base: import "${dir}/${base}.nix" }:
+    mapFilterAttrs
+      (_: v: v != null)
+      (n: v:
+        if n != "default.nix" && hasSuffix ".nix" n && v == "regular"
+        then
+          let name = removeSuffix ".nix" n; in nameValuePair (name) (_import name)
+        else
+          nameValuePair ("") (null))
+      (readDir dir);
+
+  modules =
+    let
+      # binary cache
+      cachix = import ../cachix.nix;
+      cachixAttrs = { inherit cachix; };
+
+      # modules
+      moduleList = import ../modules/list.nix;
+      modulesAttrs = pathsToImportedAttrs moduleList;
+
+      # profiles
+      profilesList = import ../profiles/list.nix;
+      profilesAttrs = { profiles = pathsToImportedAttrs profilesList; };
+    in
+    recursiveUpdate
+      (recursiveUpdate cachixAttrs modulesAttrs)
+      profilesAttrs;
+
+  genPackages = { overlay, overlays, pkgs }:
+    let
+      packages = overlay pkgs pkgs;
+      overlays' = lib.filterAttrs (n: v: n != "pkgs") overlays;
+      overlayPkgs =
+        genAttrs
+          (attrNames overlays')
+          (name: (overlays'."${name}" pkgs pkgs)."${name}");
+    in
+    recursiveUpdate packages overlayPkgs;
 
 }
