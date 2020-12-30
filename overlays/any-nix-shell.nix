a4409f79e41cfddcf9ebfc4e3412f4300f5a5830
Timothy DeHerrera <tim.deh@pm.me>
2020-12-29 20:16:26 -0700

    any-nix-shell: update overlay

--- overlays/any-nix-shell.nix
+++ overlays/any-nix-shell.nix
@@ -1,6 +1,11 @@
 final: prev: {
   any-nix-shell = prev.any-nix-shell.overrideAttrs
     (o: {
-      patches = [ ../profiles/develop/zsh/patches/0001-nix-run-is-now-nix-shell-semantically.patch ];
+      src = prev.fetchFromGitHub {
+        owner = "haslersn";
+        repo = "any-nix-shell";
+        rev = "e6b86e6e6d86cf7bcbc2691596d54d0a1db29d29";
+        hash = "sha256-ZmMl58sYVj5TVw19nxPht5nAv9qWnIyImRhs1/TrsRc=";
+      };
     });
 }
