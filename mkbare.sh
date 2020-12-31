33d70a14b026622e3dc8c1d8b2d27e0fc2722496
Timothy DeHerrera <tim.deh@pm.me>
2020-12-31 15:04:01 -0700

    script to make bare

---
+++ mkbare.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -i zsh
+
+setopt extendedglob
+setopt nullglob
+
+rm -rf profiles/^(core|user)
+rm -rf overlays/*
+rm -rf cachix/*
+rm -rf hosts/^(default.nix|niximg.nix|NixOS.nix)
+rm -rf modules/*
+rm -rf pkgs/*
+rm -rf secrets
+rm -rf users/^root
+rm -rf *.md
+rm -rf .github
+rm -rf COPYING
+
+mkdir -p users/profiles
+mkdir -p users/nixos
+
+echo "[ ./core ]" > profiles/list.nix
+echo "[ ]" > modules/list.nix
+echo "pkgs:\nwith pkgs;\n[ ]" > pkgs/override.nix
+echo "final: prev: { }" > pkgs/default.nix
+echo "final: prev: { hello = prev.hello; }" > overlays/hello.nix
+echo "{ }" > cachix/default.nix
+
+cat << EOF > users/nixos/default.nix
+{
+  users.users.nixos = {
+    uid = 1000;
+    password = "nixos";
+    description = "default";
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
+}
+EOF
+
