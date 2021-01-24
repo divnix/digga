b3ec025f2424356957f9a6e4331d86a2f05a4196
Timothy DeHerrera <tim.deh@pm.me>
2021-01-24 11:06:14 -0700

    ci: remove broken qutebrowser platforms

--- overlays/qutebrowser.nix
+++ overlays/qutebrowser.nix
@@ -1,6 +1,7 @@
 final: prev: {
   qutebrowser = prev.qutebrowser.overrideAttrs
-    (o: {
-      patches = o.patches ++ [ ../profiles/graphical/qutebrowser/0001-Fix-gopass-mime-format-for-qute-pass-userscript.patch ];
+    (self: {
+      patches = self.patches ++ [ ../profiles/graphical/qutebrowser/0001-Fix-gopass-mime-format-for-qute-pass-userscript.patch ];
+      meta.platforms = prev.lib.filter (platform: ! prev.lib.elem platform [ "aarch64-linux" "i686-linux" ]) self.meta.platforms;
     });
 }
