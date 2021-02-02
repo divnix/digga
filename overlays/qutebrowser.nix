f8e3b70aee71fd20be87f17cc534ed2d52265a4b
Timothy DeHerrera <tim.deh@pm.me>
2021-02-02 01:34:19 -0700

    qutebrowser: patch merged upstream

--- overlays/qutebrowser.nix
+++ overlays/qutebrowser.nix
@@ -1,7 +1,6 @@
 final: prev: {
   qutebrowser = prev.qutebrowser.overrideAttrs
     (self: {
-      patches = self.patches ++ [ ../profiles/graphical/qutebrowser/0001-Fix-gopass-mime-format-for-qute-pass-userscript.patch ];
       meta.platforms = prev.lib.filter (platform: ! prev.lib.elem platform [ "aarch64-linux" "i686-linux" ]) self.meta.platforms;
     });
 }
