f5c3194e44b6839995be79d8232789d66607d158
Timothy DeHerrera <tim.deh@pm.me>
2021-02-03 20:57:41 -0700

    fix: https://github.com/hercules-ci/support/issues/51

--- nix/ci.nix
+++ nix/ci.nix
@@ -1,7 +1,7 @@
 let
   inherit (default.inputs.nixos.lib) recurseIntoAttrs;
 
-  default = (import ../compat).defaultNix;
+  default = (import "${../.}/compat").defaultNix;
 
   packages = import ../default.nix;
 
