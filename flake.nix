77296ff433c0dc6e9b5cad522b6a46dafaa204ba
Timothy DeHerrera <tim.deh@pm.me>
2021-02-15 11:24:43 -0700

    deploy: move logic to lib

--- flake.nix
+++ flake.nix
@@ -40,9 +40,9 @@
     }:
     let
       inherit (utils.lib) eachDefaultSystem flattenTreeSystem;
-      inherit (nixos.lib) recursiveUpdate mapAttrs;
+      inherit (nixos.lib) recursiveUpdate;
       inherit (self.lib) overlays nixosModules genPackages genPkgs
-        genHomeActivationPackages;
+        genHomeActivationPackages mkNodes;
 
       extern = import ./extern { inherit inputs; };
 
@@ -72,16 +72,7 @@
 
           defaultTemplate = self.templates.flk;
 
-          deploy.nodes = mapAttrs
-            (_: config: {
-              hostname = config.config.networking.hostName;
-
-              profiles.system = {
-                user = "root";
-                path = deploy.lib.x86_64-linux.activate.nixos config;
-              };
-            })
-            self.nixosConfigurations;
+          deploy.nodes = mkNodes deploy self.nixosConfigurations;
 
           checks = builtins.mapAttrs
             (system: deployLib: deployLib.deployChecks self.deploy)
