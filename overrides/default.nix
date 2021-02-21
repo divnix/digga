62374ccb66078f4f09d773fcd4e6316d609ba047
David Arnold <dar@xoe.solutions>
2021-02-21 16:20:33 -0500

    wip: attempt to backport

--- overrides/default.nix
+++ overrides/default.nix
@@ -1,7 +1,14 @@
 # override defaults to nixpkgs/master
 {
   # modules to pull from override, stable version is automatically disabled
-  modules = [ ];
+  modules = [
+    "installer/sd-card/sd-image.nix"
+    "installer/sd-card/sd-image-raspberrypi4.nix"
+    "installer/sd-card/sd-image-raspberrypi.nix"
+    "installer/sd-card/sd-image-aarch64.nix"
+    "installer/sd-card/sd-image-aarch64-new-kernel.nix"
+    "installer/sd-card/sd-image-armv7l-multiplatform.nix"
+  ];
 
   # if a modules name changed in override, add the old name here
   disabledModules = [ ];
