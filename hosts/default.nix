efe90d47cd7ec315b154d54641fb58c5c2abf19a
Timothy DeHerrera <tim.deh@pm.me>
2021-03-11 23:11:34 -0700

    hosts: refactor to allow integration tests
    Lay the groundwork for easily testing profiles and modules via
    `pkgs.nixosTest` and the new `testModule`.

--- hosts/default.nix
+++ hosts/default.nix
@@ -2,7 +2,6 @@
 , home
 , lib
 , nixos
-, nixos-hardware
 , override
 , pkgs
 , self
@@ -14,76 +13,85 @@ let
 
   suites = import ../suites { inherit lib; };
 
-  config = hostName:
-    dev.os.devosSystem {
-      inherit system;
-
-      specialArgs = extern.specialArgs // { inherit suites; };
-
-      modules =
+  modules =
+    let
+      core = ../profiles/core;
+      modOverrides = { config, overrideModulesPath, ... }:
         let
-          core = ../profiles/core;
-
-          modOverrides = { config, overrideModulesPath, ... }:
-            let
-              overrides = import ../overrides;
-              inherit (overrides) modules disabledModules;
-            in
-            {
-              disabledModules = modules ++ disabledModules;
-              imports = map
-                (path: "${overrideModulesPath}/${path}")
-                modules;
-            };
-
-          global = {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-
-            hardware.enableRedistributableFirmware = lib.mkDefault true;
-
-            networking.hostName = hostName;
-
-            nix.nixPath = [
-              "nixpkgs=${nixos}"
-              "nixos-config=${self}/compat/nixos"
-              "home-manager=${home}"
-            ];
-
-            nixpkgs = { inherit pkgs; };
-
-            nix.registry = {
-              devos.flake = self;
-              nixos.flake = nixos;
-              override.flake = override;
-            };
-
-            system.configurationRevision = lib.mkIf (self ? rev) self.rev;
-          };
-
-          local = {
-            require = [
-              "${toString ./.}/${hostName}.nix"
-            ];
-          };
-
-          # Everything in `./modules/list.nix`.
-          flakeModules =
-            builtins.attrValues self.nixosModules;
-
+          overrides = import ../overrides;
+          inherit (overrides) modules disabledModules;
         in
-        flakeModules ++ [
-          core
-          global
-          local
-          modOverrides
-        ] ++ extern.modules;
+        {
+          disabledModules = modules ++ disabledModules;
+          imports = map
+            (path: "${overrideModulesPath}/${path}")
+            modules;
+        };
 
+      global = {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+
+        hardware.enableRedistributableFirmware = lib.mkDefault true;
+
+        nix.nixPath = [
+          "nixpkgs=${nixos}"
+          "nixos-config=${self}/compat/nixos"
+          "home-manager=${home}"
+        ];
+
+        nixpkgs = { inherit pkgs; };
+
+        nix.registry = {
+          devos.flake = self;
+          nixos.flake = nixos;
+          override.flake = override;
+        };
+
+        system.configurationRevision = lib.mkIf (self ? rev) self.rev;
+      };
+
+      # Everything in `./modules/list.nix`.
+      flakeModules =
+        builtins.attrValues self.nixosModules;
+
+    in
+    flakeModules ++ [
+      core
+      global
+      modOverrides
+    ] ++ extern.modules;
+
+  specialArgs = extern.specialArgs // { inherit suites; };
+
+  mkHostConfig = hostName:
+    let
+      local = {
+        require = [
+          "${toString ./.}/${hostName}.nix"
+        ];
+
+        networking = { inherit hostName; };
+      };
+    in
+    dev.os.devosSystem {
+      inherit system specialArgs;
+
+      modules = modules ++ [
+        local
+        {
+          lib.testModule = {
+            imports = modules;
+            _module.args = specialArgs;
+          };
+        }
+      ];
     };
 
-  hosts = dev.os.recImport {
-    dir = ./.;
-    _import = config;
-  };
+  hosts = dev.os.recImport
+    {
+      dir = ./.;
+      _import = mkHostConfig;
+    };
 in
 hosts
