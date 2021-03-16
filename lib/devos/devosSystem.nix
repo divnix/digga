a09c1a982fc07c5b2cb178973398c94c63114431
David Arnold <dar@xoe.solutions>
2021-03-15 20:58:11 -0500

    lib/devos: bake bootstaping link-local network into live iso
    Replace default networking with a specially configured systemd-networkd
    network which is responsible for setting up DHCP and also provide a well
    known ipv6 link-local address as well as enable multicastDNS to expose
    the hostname on the local link over the reserved `local.` top level domain.

--- lib/devos/devosSystem.nix
+++ lib/devos/devosSystem.nix
@@ -22,6 +22,34 @@ lib.nixosSystem (args // {
               networking.networkmanager.enable = lib.mkForce false;
               # confilcts with networking.wireless
               networking.wireless.iwd.enable = lib.mkForce false;
+              # Set up a link-local boostrap network
+              # See also: https://github.com/NixOS/nixpkgs/issues/75515#issuecomment-571661659
+              networking.usePredictableInterfaceNames = lib.mkForce true; # so prefix matching works
+              networking.useNetworkd = lib.mkForce true;
+              networking.useDHCP = lib.mkForce false;
+              networking.dhcpcd.enable = lib.mkForce false;
+              systemd.network = {
+                # https://www.freedesktop.org/software/systemd/man/systemd.network.html
+                networks."boostrap-link-local" = {
+                  matchConfig = {
+                    Name = "en* wl* ww*";
+                  };
+                  networkConfig = {
+                    Description = "Link-local host bootstrap network";
+                    MulticastDNS = true;
+                    LinkLocalAddressing = "ipv6";
+                    DHCP = "yes";
+                  };
+                  address = [
+                    # fall back well-known link-local for situations where MulticastDNS is not available
+                    "fe80::47" # 47: n=14 i=9 x=24; n+i+x
+                  ];
+                  extraConfig = ''
+                    # Unique, yet stable. Based off the MAC address.
+                    IPv6LinkLocalAddressGenerationMode = "eui64"
+                  '';
+                };
+              };
             })
           ];
         })).config;
