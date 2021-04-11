a34254072f5fa772961ac203ab238102de0b5313
Pacman99 <Pacman99@users.noreply.github.com>
2021-04-11 05:07:19 +0000

    deploy: 8ba7bffc3f0960fddd864a9c1760b8b439d55d57

--- lib/devos/mkHomeConfigurations.nix
+++ lib/devos/mkHomeConfigurations.nix
@@ -3,7 +3,7 @@
 with lib;
 let
   mkHomes = host: config:
-    mapAttrs' (user: v: nameValuePair "${user}@${host}" v)
+    mapAttrs' (user: v: nameValuePair "${user}@${host}" v.home)
       config.config.system.build.homes;
 
   hmConfigs = mapAttrs mkHomes self.nixosConfigurations;
