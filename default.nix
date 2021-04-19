db5977337b0a1ae30b153f20a9f3be7af5dca969
nrdxp <nrdxp@users.noreply.github.com>
2021-04-19 03:33:12 +0000

    deploy: 9eeddb8dacff9a5f6f4ec2645247160c074aaca3

--- default.nix
+++
@@ -1,8 +0,0 @@
-let
-  inherit (default.inputs.nixos) lib;
-
-  default = (import ./compat).defaultNix;
-in
-builtins.mapAttrs (_: v: lib.recurseIntoAttrs v) default.packages // {
-  shell = import ./shell.nix;
-}
--- shell.nix
+++
@@ -1,1 +0,0 @@
-(import ./compat).shellNix
