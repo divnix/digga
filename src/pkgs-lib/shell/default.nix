8b77cd1d5ed07e4d57cb2e850ce854c467296c35
Pacman99 <pachum99@myrdd.info>
2021-05-18 08:08:06 -0700

    Revert "shell: add packages from overlays to devshell"
    This reverts commit be058ad58dd0e43c46138a073e94ca3c313401be.

--- src/pkgs-lib/shell/default.nix
+++ src/pkgs-lib/shell/default.nix
@@ -17,7 +17,10 @@ let
     })
   ];
 
-  pkgs' = pkgs.appendOverlays overlays;
+  pkgs' = import pkgs.path {
+    inherit (pkgs) system;
+    inherit overlays;
+  };
 
   flk = pkgs'.callPackage ./flk.nix { };
 
@@ -25,49 +28,35 @@ let
     inherit (pkgs') system;
     modules = [ ];
   }).config.system.build;
-
-  # Add all packages from overlays to shell
-  # Follow same logic as exporting packages except don't filter out inputs
-  allOverlays = lib.exporters.internalOverlays {
-    # function requires system-spaced and channel-spaced package set
-    pkgs.${pkgs'.system}.channel = pkgs';
-  };
-  customPackages = lib.exporters.fromOverlays allOverlays { pkgs = pkgs'; };
-
-  configuration = {
-    imports = [ (pkgs'.devshell.importTOML ./devshell.toml) ] ++ extraModules;
-
-    packages = with installPkgs; [
-      nixos-install
-      nixos-generate-config
-      nixos-enter
-    ] ++ (builtins.attrValues customPackages);
-
-    git.hooks = {
-      pre-commit.text = lib.fileContents ./pre-commit.sh;
-    };
-
-    commands = with pkgs'; [
-      { package = flk; }
-      {
-        name = "nix";
-        help = pkgs'.nixFlakes.meta.description;
-        command = ''
-          ${pkgs'.nixFlakes}/bin/nix --experimental-features "nix-command flakes ca-references" "${"\${@}"}"
-        '';
-      }
-    ]
-    ++ lib.optional (system != "i686-linux") { package = cachix; }
-    ++ lib.optional (system == "x86_64-linux") {
-      name = "deploy";
-      package = deploy-rs;
-      help = "A simple multi-profile Nix-flake deploy tool.";
-    };
-  };
 in
-(pkgs'.devshell.eval {
-  inherit configuration;
-  # Allows us to use devshell's `importTOML` within a module
-  # used in evalArgs to auto-import toml files
-  extraSpecialArgs.pkgs = pkgs';
-}).shell
+pkgs'.devshell.mkShell {
+  imports = [ (pkgs'.devshell.importTOML ./devshell.toml) ];
+
+  packages = with installPkgs; [
+    nixos-install
+    nixos-generate-config
+    nixos-enter
+    pkgs'.nixos-rebuild
+  ];
+
+  git.hooks = {
+    pre-commit.text = lib.fileContents ./pre-commit.sh;
+  };
+
+  commands = with pkgs'; [
+    { package = flk; }
+    {
+      name = "nix";
+      help = pkgs'.nixFlakes.meta.description;
+      command = ''
+        ${pkgs'.nixFlakes}/bin/nix --experimental-features "nix-command flakes ca-references" "${"\${@}"}"
+      '';
+    }
+  ]
+  ++ lib.optional (system != "i686-linux") { package = cachix; }
+  ++ lib.optional (system == "x86_64-linux") {
+    name = "deploy";
+    package = deploy-rs;
+    help = "A simple multi-profile Nix-flake deploy tool.";
+  };
+}
