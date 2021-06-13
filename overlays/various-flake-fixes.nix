e1d7178979a421bbfc83961edb962871acf5c791
David Arnold <dar@xoe.solutions>
2021-06-13 12:15:09 -0500

    init: common flake compat fixes for other nixpkgs
    fixes #315

---
+++ overlays/various-flake-fixes.nix
@@ -0,0 +1,9 @@
+final: prev: {
+  # Since: https://github.com/NixOS/nixpkgs/pull/126137
+  nix-direnv =
+    if builtins.hasAttr "enableFlakes" prev.nix-direnv.override.__functionArgs
+    then prev.nix-direnv.override {
+      enableFlakes = true;
+    }
+    else prev.nix-direnv;
+}
