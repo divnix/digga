5a16e8f6e88e3cbe18c24cb83ba20bbdf013feb7
David Arnold <dgx.arnold@gmail.com>
2021-06-19 14:28:24 -0500

    use config.lib.digga.mkBuild

--- tests/fullFlake/modules/customBuilds.nix
+++ tests/fullFlake/modules/customBuilds.nix
@@ -1,30 +1,26 @@
 { lib, pkgs, self, config, modules, channel, ... }:
-let
-  mkBuild = buildModule:
-    channel.input.lib.nixosSystem {
-      inherit (pkgs) system;
-      inherit (config.lib) specialArgs;
-      modules = modules ++ [ buildModule ];
-    };
-in
 {
   system.build = {
-    iso = (mkBuild (config.lib.digga.modules.isoConfig {
-      inherit self;
-      fullHostConfig = config;
-    })).config.system.build.isoImage;
+    iso = (config.lib.digga.mkBuild
+      (config.lib.digga.modules.isoConfig {
+        inherit self;
+        fullHostConfig = config;
+      })
+    ).config.system.build.isoImage;
 
-    homes = (mkBuild ({ config, ... }: {
-      home-manager.useUserPackages = lib.mkForce false;
-      home-manager.sharedModules = [
-        {
-          home.sessionVariables = {
-            inherit (config.environment.sessionVariables) NIX_PATH;
-          };
-          xdg.configFile."nix/registry.json".text =
-            config.environment.etc."nix/registry.json".text;
-        }
-      ];
-    })).config.home-manager.users;
+    homes = (config.lib.digga.mkBuild
+      ({ config, ... }: {
+        home-manager.useUserPackages = lib.mkForce false;
+        home-manager.sharedModules = [
+          {
+            home.sessionVariables = {
+              inherit (config.environment.sessionVariables) NIX_PATH;
+            };
+            xdg.configFile."nix/registry.json".text =
+              config.environment.etc."nix/registry.json".text;
+          }
+        ];
+      })
+    ).config.home-manager.users;
   };
 }
