9006a2a89e05dd1469067525f7ac825c7ff624bd
Pacman99 <pachum99@myrdd.info>
2021-07-03 10:48:35 -0700

    importers: include flattenTree in internal lib

--- src/importers.nix
+++ src/importers.nix
@@ -146,7 +146,7 @@ let
 
 in
 {
-  inherit rakeLeaves;
+  inherit rakeLeaves flattenTree;
 
   mkProfileAttrs = builtins.trace ''
     The function, mkProfileAttrs, has been deprecated, you can now create profile sets
