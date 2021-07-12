4bb01ba900e1ccd754f953a7d5e76b3ae0987a35
David Arnold <dgx.arnold@gmail.com>
2021-07-12 15:17:43 -0500

    fix: add the entirety of nix#4641 patch
    Previously, a specific commit was references, while the patch has +-
    evolved into its final shape, which we want to capture here.

--- shell.nix
+++ shell.nix
@@ -63,8 +63,8 @@ let
     patches = (o.patches or [ ]) ++ [
       (pkgs.fetchpatch {
         name = "fix-follows.diff";
-        url = "https://github.com/CitadelCore/nix/commit/cfef23c040c950222b3128b9da464d9fe6810d79.diff";
-        sha256 = "sha256-KpYSX/k7FQQWD4u4bUPFOUlPV4FyfuDy4OhgDm+bkx0=";
+        url = "https://patch-diff.githubusercontent.com/raw/NixOS/nix/pull/4641.patch";
+        sha256 = "sha256-nyLMSltS9XjNaF446M5yV/o08XtZkYbU7yMVnqYERts=";
       })
     ];
   });
