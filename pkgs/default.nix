6d05060a8b8a0ea35ee774ba302d48fcb5508c50
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-07-25 23:51:14 +0100

    Start collecting rules for auto building pkgSets

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,29 @@
-final: prev: {
-  # keep sources this first
-  sources = prev.callPackage (import ./_sources/generated.nix) { };
-  # then, call packages with `final.callPackage`
+self: super:
+let
+  sources = (import ./_sources/generated.nix) { inherit (self) fetchurl fetchgit; };
+
+  mkVimPlugin = plugin:
+    self.vimUtils.buildVimPluginFrom2Nix {
+      inherit (plugin) pname version src;
+    };
+
+  newPkgsSet = pkgSet:
+    let
+      prefix = "${pkgSet}-";
+
+      pkgSetBuilder = {
+        "vimPlugins" = mkVimPlugin;
+      }.${pkgSet};
+
+
+      pkgsInSources = self.lib.mapAttrs' (name: value: self.lib.nameValuePair (self.lib.removePrefix prefix name) (value)) (self.lib.filterAttrs (n: v: self.lib.hasPrefix prefix n) sources);
+    in
+    self.lib.mapAttrs (n: v: pkgSetBuilder v) pkgsInSources;
+
+in
+{
+  inherit sources;
+
+  vimPlugins = super.vimPlugins // (newPkgsSet "vimPlugins");
+
 }
