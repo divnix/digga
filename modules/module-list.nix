5bd943f97da8e63c4b8a6c234d71fcdb7b7e7193
David Arnold <dgx.arnold@gmail.com>
2021-07-31 23:03:19 -0500

    imp: advise removal of */modules-list.nix on merge conflicts
    During merge conflicts, these lines should appear in the hunks
    and give instructions on how to handle them in the face of the
    implementation of `rakeLeaves` for modules in https://git.io/JB1Wk via
    `importModules`, that knows how to rake leaves by walking the folder
    tree on it's own and collecting leaves that are normal nix files or
    folers who contain a `default.nix` mmicking strongly a "recursive"
    import-ish syntax.

--- modules/module-list.nix
+++ modules/module-list.nix
@@ -1,1 +1,2 @@
+# ON MERGE CONFLICT: remove this file when updating to the use of digga.lib.importModules in flake.nix: importModules knows how to rakes leaves by walking the file tree on its own. see commit: https://git.io/JB1Wk
 [ ]
