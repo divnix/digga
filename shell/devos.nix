f10a8fdd33e5b354e6c5cfe32029900a891e65ca
GTrunSec <gtrunsec@hardenedlinux.org>
2021-09-29 11:49:43 -0700

    chore: update nvfecher

--- shell/devos.nix
+++ shell/devos.nix
@@ -41,7 +41,7 @@ in
       category = "devos";
       name = pkgs.nvfetcher-bin.pname;
       help = pkgs.nvfetcher-bin.meta.description;
-      command = "cd $DEVSHELL_ROOT/pkgs; ${pkgs.nvfetcher-bin}/bin/nvfetcher -c ./sources.toml --no-output $@";
+      command = "cd $PRJ_ROOT/pkgs; ${pkgs.nvfetcher-bin}/bin/nvfetcher -c ./sources.toml $@";
     }
     (linter nixpkgs-fmt)
     (linter editorconfig-checker)
