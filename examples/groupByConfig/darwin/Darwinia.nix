fbfcbd81b53ebd0343e572c1ff2a0c3d6e2f1659
Chris Montgomery <chris@cdom.io>
2022-02-27 21:16:29 -0500

    docs: comments and formatting

--- examples/groupByConfig/darwin/Darwinia.nix
+++ examples/groupByConfig/darwin/Darwinia.nix
@@ -3,6 +3,8 @@
 {
   imports = suites.base;
 
+  # On darwin, sudoers/admins are added to the `admin` group, not `wheel` as
+  # they would be on Linux.
   nix.trustedUsers = [ "@admin" "sosumi" ];
 
   # https://daiderd.com/nix-darwin/manual/index.html#opt-system.stateVersion
