3e19251e0515f72b02db2c9f8c4055323554a994
teutat3s <10206665+teutat3s@users.noreply.github.com>
2022-04-09 16:32:51 +0200

    pre-commit hook: only format modified lines
    instead of entire files

--- examples/devos/shell/hooks/pre-commit.sh
+++ examples/devos/shell/hooks/pre-commit.sh
@@ -5,7 +5,7 @@ then
   against=HEAD
 else
   # Initial commit: diff against an empty tree object
-  against=$(${git}/bin/git hash-object -t tree /dev/null)
+  against=$(git hash-object -t tree /dev/null)
 fi
 
 diff="git diff-index --name-only --cached $against --diff-filter d"
@@ -15,8 +15,19 @@ mapfile -t all_files < <($diff)
 
 # Format staged nix files.
 if [[ -n "${nix_files[@]}" ]]; then
+  # stash only unstaged changes, keeping staged changes
+  old_stash=$(git rev-parse --quiet --verify refs/stash)
+  git stash push --quiet --keep-index -m 'Unstaged changes before pre-commit hook'
+  new_stash=$(git rev-parse --quiet --verify refs/stash)
+
+  # format staged changes
   nixpkgs-fmt "${nix_files[@]}" \
   && git add "${nix_files[@]}"
+
+  # if unstaged changes were stashed reapply to working tree
+  if [ "$old_stash" != "$new_stash" ]; then
+      git stash pop --quiet
+  fi
 fi
 
 # check editorconfig
