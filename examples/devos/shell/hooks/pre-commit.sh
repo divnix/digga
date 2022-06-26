cb8e55d4a47091c8772229d6cf24b4cca68ac28a
teutat3s <10206665+teutat3s@users.noreply.github.com>
2022-06-27 00:54:16 +0200

    pre-commit hook: fix comment formatting

--- examples/devos/shell/hooks/pre-commit.sh
+++ examples/devos/shell/hooks/pre-commit.sh
@@ -13,24 +13,24 @@ diff="git diff-index --name-only --cached $against --diff-filter d"
 mapfile -t nix_files < <($diff -- '*.nix')
 mapfile -t all_files < <($diff)
 
-# Format staged nix files.
+# Format staged nix files
 if [[ -n "${nix_files[@]}" ]]; then
-  # stash only unstaged changes, keeping staged changes
+  # Stash only unstaged changes, keeping staged changes
   old_stash=$(git rev-parse --quiet --verify refs/stash)
   git stash push --quiet --keep-index -m 'Unstaged changes before pre-commit hook'
   new_stash=$(git rev-parse --quiet --verify refs/stash)
 
-  # format staged changes
+  # Format staged changes
   nixpkgs-fmt "${nix_files[@]}" \
   && git add "${nix_files[@]}"
 
-  # if unstaged changes were stashed reapply to working tree
+  # If unstaged changes were stashed re-apply to working tree
   if [ "$old_stash" != "$new_stash" ]; then
       git stash pop --quiet
   fi
 fi
 
-# check editorconfig
+# Check editorconfig
 if ! editorconfig-checker -- "${all_files[@]}"; then
   printf "%b\n" \
     "\nCode is not aligned with .editorconfig" \
