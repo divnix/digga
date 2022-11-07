9c068a7ce5099400e1d3401ae58796db576d65b2
Lord-Valen <lord_valen@protonmail.com>
2022-11-06 22:45:02 -0500

    :brain: `redesign` Rm tempfix

--- devShell.nix
+++ devShell.nix
@@ -86,21 +86,6 @@ devshell.mkShell {
     }
   ];
 
-  # tempfix: remove when merged https://github.com/numtide/devshell/pull/123
-  devshell.startup.load_profiles = pkgs.lib.mkForce (pkgs.lib.noDepEntry ''
-    # PATH is devshell's exorbitant privilige:
-    # fence against its pollution
-    _PATH=''${PATH}
-    # Load installed profiles
-    for file in "$DEVSHELL_DIR/etc/profile.d/"*.sh; do
-      # If that folder doesn't exist, bash loves to return the whole glob
-      [[ -f "$file" ]] && source "$file"
-    done
-    # Exert exorbitant privilige and leave no trace
-    export PATH=''${_PATH}
-    unset _PATH
-  '');
-
   commands = [
     (utils {
       command = "git rm --ignore-unmatch -f $PRJ_ROOT/{tests,examples}/*/flake.lock";
